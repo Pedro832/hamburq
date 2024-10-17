a03c15020c9e75b1666951b3f6e4d0d73ef3ed5f
Pedro Kauã <115954424+Pedro832@users.noreply.github.com>
2024-10-17 18:16:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,88 +1,92 @@
-const menu = document.getElementById("menu")
-const cartBtn = document.getElementById("cart-btn")
-const cartModal = document.getElementById("cart-modal")
-const cartItemsContainer = document.getElementById("cart-items")
-const cartTotal = document.getElementById("cart-total")
-const checkoutBtn = document.getElementById("checkout-btn")
-const closeModalBtn = document.getElementById("close-modal-btn")
-const cartCounter = document.getElementById("cart-counter")
-const addressInput= document.getElementById("address")
-const addressWarn = document.getElementById("address-warn")
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const checkoutBtn = document.getElementById("checkout-btn");
+const closeModalBtn = document.getElementById("close-modal-btn");
+const cartCounter = document.getElementById("cart-counter");
+const addressInput = document.getElementById("address");
+const addressWarn = document.getElementById("address-warn");
 
 
 let cart = [];
-cartBtn.addEventListener("click", function() {
-    cartModal.style.display = "flex"
-    updateCartModal()
-})
+cartBtn.addEventListener("click", function () {
+  cartModal.style.display = "flex";
+  updateCartModal();
+});
 
-cartModal.addEventListener("click", function(event){
-    if(event.target === cartModal){
-        cartModal.style.display = "none"
-    }
-})
+cartModal.addEventListener("click", function (event) {
+  if (event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
+});
 
-closeModalBtn.addEventListener("click", function(){
-    cartModal.style.display = "none"    
-})
+closeModalBtn.addEventListener("click", function () {
+  cartModal.style.display = "none";
+});
 
-menu.addEventListener("click", function(event){
+menu.addEventListener("click", function (event) {
+  let parentButton = event.target.closest(".add-to-cart-btn");
+  if (parentButton) {
+    const name = parentButton.getAttribute("data-name");
+    const price = parseFloat(parentButton.getAttribute("data-price"));
+  }
+});
 
-    let parentButton = event.target.closest(".add-to-cart-btn")
-    if(parentButton){
-        const name = parentButton.getAttribute("data-name")
-        const pice = parseFloat(parentButton.getAttribute("data-price"))
+function addToCart(name, price) {
+  const existingItem = cart.find((item) => item.name === name);
 
+  
 
-    }
-})
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    });
+  }
 
-function addToCart(name, price){
-    const existingItem = cart.find(item =>name === name)
-
-    if(existingItem){
-
-        existingItem.quantity += 1;
-    }else{
-        
-        cart.push({
-            name,
-            price,
-            quantity: 1,
-        })
-    }
-
-    updateCartModal()
+  updateCartModal();
+  
 }
 
-function updateCartModal(){
+function updateCartModal() {
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
 
-    cartItemsContainer.innerHTML = "";
-    let total = 0;
+  cart.forEach((item) => {
+     const cartItemElement = document.createElement("div");
+    cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col" )
 
-    cart.forEach(item =>{
-        const cartItemElement = document.createElement("div")
-
-        cartItemElement.innerHTML = `
-        <div>
+    cartItemElement.innerHTML = `
+        <div class="flex items-center justify-between">
          <div>
-          <p>${item.name}</p>
+          <p class="font-medium">${item.name}</p>
           <p>${item.quantity}</p>
-          <p>R$ ${item.price}</p>
+          <p class="font-medium mt-2">R$ ${item.price}</p>
          </div>
 
          <div>
-          <button>
+          <button class="remove-from-cart-btn" data-name="${item.name}">
             Remover
           </button>
          </div>
 
     
-        </div>
-        `
-        
-        cartItemsContainer.appendChild(cartItemElement)
+          </div>
+        `;
 
-    })
+        total += item.price * item.quantity;
+
+    cartItemsContainer.appendChild(cartItemElement);
+  });
+
+  cartTotal.textContent = total.toLocaleString("pt-BR", {
+   style: "currency",
+   currency: "BRL"
+  })
 
 }
